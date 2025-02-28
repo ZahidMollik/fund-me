fa7f41eae6b86e2e60026e63690c53ad1dc1f107
ZahidMollik <md.zahidulmollik@gmail.com>
2025-02-28 16:56:14 +0600

    Created price converter library and made withdraw method for owner only

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.26;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./priceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
     uint256 public MINIMUM_USD=5e18;
     address[] public funders;
     mapping(address=>uint256)public fundersToBalance;
@@ -13,35 +15,27 @@ contract FundMe{
         owner=msg.sender;
     }
 
+    modifier onlyOwner(){
+        require(msg.sender==owner,"only owner can access");
+        _;
+    }
+
     function fund()public payable {
-        require(getConversionRate(msg.value)>=MINIMUM_USD,"not enough eth");
+        require(msg.value.getConversionRate()>=MINIMUM_USD,"You need to spend more ETH");
         funders.push(msg.sender);
         fundersToBalance[msg.sender]+=msg.value;
     }
 
-    function withdraw()public{
+    function withdraw()public onlyOwner{
         for(uint256 index=0;index<funders.length;index++){
             address funder=funders[index];
             fundersToBalance[funder]=0;
         }
 
-         funders=new address[](0);
+        funders= new address[](0);
 
          (bool success,)=payable(msg.sender).call{value:address(this).balance}("");
          require(success,"fail to withdraw");
-    }
-
-    function getPrice() public view returns(uint256) {
-        //ETH/USD address- 0x694AA1769357215DE4FAC081bf1f309aDC325306
-       AggregatorV3Interface priceFeed=AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-       (,int256 price,,,)=priceFeed.latestRoundData();
-       return uint256(price*1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice=getPrice();
-        uint256 ethAmountUsd=(ethAmount*ethPrice)/1e18;
-        return ethAmountUsd;
     }
 
     function getVersion() public view returns (uint256){
